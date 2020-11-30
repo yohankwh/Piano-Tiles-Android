6f9827a61709ae3036c57fdfddd5eba94380824e
edwin <edwinpranajaya0703@gmail.com>
2020-11-30 23:08:12 +0700

    create rectangles

--- app/src/main/java/com/example/tubes02/MainPresenter.java
+++ app/src/main/java/com/example/tubes02/MainPresenter.java
@@ -2,8 +2,14 @@ package com.example.tubes02;
 
 public class MainPresenter {
     private FragmentListener fl;
+    private Rectangles[] rects;
 
     public MainPresenter(FragmentListener fl){
         this.fl = fl;
+
+
+        rects = new Rectangles[3];
     }
+
+
 }
