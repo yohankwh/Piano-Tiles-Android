d3fdd69f3c29a58dff8361969374a64ea0a8aa66
NaomiShinna <myrichardkun1@gmail.com>
2020-12-03 15:05:36 +0700

    [UPDATE]Lagu OnPlay/start sudah jalan

--- app/src/main/java/com/example/tubes02/HomeFragment.java
+++ app/src/main/java/com/example/tubes02/HomeFragment.java
@@ -1,6 +1,8 @@
 package com.example.tubes02;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,7 +15,7 @@ import androidx.fragment.app.Fragment;
 public class HomeFragment extends Fragment implements View.OnClickListener {
     private Button startBtn;
     private FragmentListener fragmentListener;
-
+    private Button extBtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -21,6 +23,8 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         this.startBtn = view.findViewById(R.id.start_btn);
         this.startBtn.setOnClickListener(this);
+        this.extBtn = view.findViewById(R.id.ext_btn);
+        this.extBtn.setOnClickListener(this);
 
         return view;
     }
@@ -40,6 +44,28 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
     public void onClick(View view) {
         if(view==this.startBtn){
             this.fragmentListener.changePage(2);
+        }else if(view == this.extBtn){
+            Log.d("debug", "tombol keluar");
+
+            AlertDialog.Builder builderAlert = new AlertDialog.Builder(getActivity());
+            builderAlert.setTitle("Keluar")
+                    .setMessage("Apakah kamu ingin keluar?")
+                    .setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Log.d("debug", "clicked Delete");
+                            fragmentListener.closeApplication();
+                        }
+                    })
+                    .setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+//                                    Log.d("debug", "clicked tidak jadi delete");
+                            dialog.cancel();
+                        }
+                    });
+            builderAlert.create();
+            builderAlert.show();
         }
     }
 }
