b0356599832e8e5604f0bb584de79f7783c15ba2
Yohan Kurnia Wijaya <yohanhuang300999@gmail.com>
2020-12-03 14:17:17 +0700

    How to Initiate Canvas Lol

--- app/src/main/java/com/example/tubes02/GameFragment.java
+++ app/src/main/java/com/example/tubes02/GameFragment.java
@@ -1,23 +1,32 @@
 package com.example.tubes02;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 
-public class GameFragment extends Fragment implements View.OnClickListener {
+public class GameFragment extends Fragment implements View.OnClickListener, View.OnTouchListener {
     private FragmentListener fragmentListener;
-
-
+    protected Canvas mCanvas;
+    protected ImageView ivCanvas;
+    protected Bitmap mBitmap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_game, container, false);
+        this.ivCanvas = view.findViewById(R.id.iv_main);
 
         return view;
     }
@@ -33,8 +42,34 @@ public class GameFragment extends Fragment implements View.OnClickListener {
         }
     }
 
+    public void initiateCanvas(){
+        // 1. Create Bitmap
+        Log.d("width: ",this.ivCanvas.getWidth()+"");
+        Log.d("height: ",this.ivCanvas.getHeight()+"");
+        this.mBitmap = Bitmap.createBitmap(this.ivCanvas.getWidth(),this.ivCanvas.getHeight(), Bitmap.Config.ARGB_8888);
+        // 2. Associate the bitmap to the ImageView.
+        this.ivCanvas.setImageBitmap(mBitmap);
+        // 3. Create a Canvas with the bitmap.
+        this.mCanvas = new Canvas(this.mBitmap);
+        //resetCanvas
+        this.resetCanvas();
+        this.ivCanvas.setOnTouchListener(this);
+    }
+
+    public void resetCanvas(){
+        // 4. Draw canvas background
+        this.mCanvas.drawColor(ResourcesCompat.getColor(getResources(), R.color.white, null));
+        // 5. force draw
+        this.ivCanvas.invalidate();
+    }
+
     @Override
     public void onClick(View v) {
 
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        return false;
     }
 }
